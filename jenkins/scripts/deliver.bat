061bd4a2e6be107acbb0b028550faf7cd9232c1a
fantestiQ <isac3539@gmail.com>
2026-07-07 19:32:36 -0300

    corrigindo bat denovo

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -5,10 +5,10 @@ echo Jenkins's local Maven repository (and the "maven-repository" Docker data vo
 call mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
 echo Extracting artifactId and version directly from pom.xml
-for /f "tokens=2 delims=<>" %%i in ('findstr /r "<artifactId>" pom.xml ^| findstr /v "plugin"') do (
+for /f "tokens=3 delims=<>" %%i in ('findstr /r "<artifactId>" pom.xml ^| findstr /v "plugin"') do (
     if not defined NAME set "NAME=%%i"
 )
-for /f "tokens=2 delims=<>" %%i in ('findstr /r "<version>" pom.xml') do (
+for /f "tokens=3 delims=<>" %%i in ('findstr /r "<version>" pom.xml') do (
     if not defined VERSION set "VERSION=%%i"
 )
 
