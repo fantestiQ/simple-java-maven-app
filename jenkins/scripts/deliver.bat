ae61b58af51621e05a5351bfdb920fa19cffd1df
fantestiQ <isac3539@gmail.com>
2026-07-07 19:30:09 -0300

    corrigindo bat mais uma vez

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -4,16 +4,14 @@ echo into the local Maven repository, which will ultimately be stored in
 echo Jenkins's local Maven repository (and the "maven-repository" Docker data volume).
 call mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
-echo The following command extracts the value of the name element
-echo within project of your Java/Maven project's pom.xml file.
-for /f "usebackq delims=" %%i in (`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`) do set "NAME=%%i"
+echo Extracting artifactId and version directly from pom.xml
+for /f "tokens=2 delims=<>" %%i in ('findstr /r "<artifactId>" pom.xml ^| findstr /v "plugin"') do (
+    if not defined NAME set "NAME=%%i"
+)
+for /f "tokens=2 delims=<>" %%i in ('findstr /r "<version>" pom.xml') do (
+    if not defined VERSION set "VERSION=%%i"
+)
 
-echo The following command behaves similarly to the previous one but
-echo extracts the value of the version element within project instead.
-for /f "usebackq delims=" %%i in (`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`) do set "VERSION=%%i"
-
-echo The following command runs and outputs the execution of your Java
-echo application (which Jenkins built using Maven) to the Jenkins UI.
 echo NAME=%NAME%
 echo VERSION=%VERSION%
 java -jar target\%NAME%-%VERSION%.jar
